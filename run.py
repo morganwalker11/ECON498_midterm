1af63545be8e77dd79bd99a8230fac91f774aa08
morganwalker11 <78051507+morganwalker11@users.noreply.github.com>
2021-03-03 22:02:43 -0500

    Add files via upload

--- run.py
+++ run.py
@@ -8,31 +8,64 @@ import numpy
 
 df = pandas.read_csv("Crimes_-_2001_to_Present.csv", dtype='object')
 df.dropna()
-dataset = df[(df['Year'] == 2018) | (df['Year'] == 2019) | (df['Year'] == 2020) ]
-dataset = dataset.sample(frac=0.1).reset_index(drop=True)
+dataset = df[(df['Year'] == "2015") | (df['Year'] == "2018") | (df['Year'] == "2019") | (df['Year'] == "2020")]
+print(dataset)
+dataset = dataset.sample(frac=1).reset_index(drop=True)
 dataset = dataset.dropna()
-dataset.to_csv('Crimes_Sample.csv')
+#print(dataset[(dataset['Primary Type'] == "BATTERY")])
+dataset['BATTERY'] = numpy.where(dataset['Primary Type']== 'BATTERY', 1,0 )
 
-def data_imputation(dataset, impute_target_name, impute_data):
-	impute_target = dataset[impute_target_name]
-	sub_dataset = pandas.concat([impute_target, impute_data], axis = 1)
-	
-	data = sub_dataset.loc[:, sub_dataset.columns != impute_target_name].values
-	target = dataset[impute_target_name].values
 
-	kfold_object = KFold(n_splits=4)
-	kfold_object.get_n_splits(data)
-	i=0
-	for training_index, test_index in kfold_object.split(data):
-		i=i+1
-		data_training = data[training_index]
-		data_test = data[test_index]
-		target_training = target[training_index]
-		target_test = target[test_index]
-		machine = linear_model.LogisticRegression()
-		machine.fit(data_training,target_training)
-		new_target = machine.predict(data_test)
-		print("Accuracy Score: ", metrics.accuracy_score(target_test, new_target))
+dataset['Date'] = pandas.to_datetime(dataset['Date'])
+dataset['month'] = dataset['Date'].dt.month
+dataset['hour'] = dataset['Date'].dt.hour
+dataset['hour_slot'] = numpy.select([
+	(dataset['hour'] < 4),
+	(dataset['hour'] < 8),
+	(dataset['hour'] < 12),
+	(dataset['hour'] < 16),
+	(dataset['hour'] < 20),
+	(dataset['hour'] < 24)]
+	, [0,1,2,3,4,5])
+
+dataset['hour_slot_0'] = numpy.where(dataset['hour_slot']== 0, 1,0 )
+dataset['hour_slot_1'] = numpy.where(dataset['hour_slot']== 1, 1,0 )
+dataset['hour_slot_2'] = numpy.where(dataset['hour_slot']== 2, 1,0 )
+dataset['hour_slot_3'] = numpy.where(dataset['hour_slot']== 3, 1,0 )
+dataset['hour_slot_4'] = numpy.where(dataset['hour_slot']== 4, 1,0 )
+dataset['hour_slot_5'] = numpy.where(dataset['hour_slot']== 5, 1,0 )
+print(dataset)
+
+target = dataset.iloc[:,22].values
+data = dataset.iloc[:,26:32].values
+print(data)
+
+machine = linear_model.LogisticRegression()
+machine.fit(data, target)
+
+new_data = [[1,0,0,0,0,0],
+[0,1,0,0,0,0],
+[0,0,1,0,0,0],
+[0,0,0,1,0,0],
+[0,0,0,0,1,0],
+[0,0,0,0,0,1]]
+#new_target = machine.predict(new_data)
+new_target = machine.predict_proba(new_data)
+print(new_target)
+
+# 	kfold_object = KFold(n_splits=4)
+# 	kfold_object.get_n_splits(data)
+# 	i=0
+# 	for training_index, test_index in kfold_object.split(data):
+# 		i=i+1
+# 		data_training = data[training_index]
+# 		data_test = data[test_index]
+# 		target_training = target[training_index]
+# 		target_test = target[test_index]
+# 		machine = linear_model.LogisticRegression()
+# 		machine.fit(data_training,target_training)
+# 		new_target = machine.predict(data_test)
+# 		print("Accuracy Score: ", metrics.accuracy_score(target_test, new_target))
 		
 		
 
